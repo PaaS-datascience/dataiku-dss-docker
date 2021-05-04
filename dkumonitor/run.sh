eb3f461f993569e0a6e1e385c24db9206a05e5e6
pli01 <pli01@users.noreply.github.com>
2021-05-04 17:46:29 +0200

    Add restart-, fix dkumonitor entryppoint

--- dkumonitor/run.sh
+++ dkumonitor/run.sh
@@ -5,7 +5,7 @@ DKUMONITOR_INSTALLDIR="/home/dkumonitor/dkumonitor-$DKUMONITOR_VERSION"
 if [ ! -f "$DKUMONITOR_DATADIR"/bin/env-default ]; then
 	# Initialize new data directory
 	"$DKUMONITOR_INSTALLDIR"/installer -d "$DKUMONITOR_DATADIR" -p "$DKUMONITOR_PORT" -t server
-elif [ $(bash -c 'source "$DKUMONITOR_DATADIR"/bin/env-default && echo "$DKUINSTALLDIR"') != "$DKUMONITOR_INSTALLDIR" ]; then
+elif [ "$(bash -c 'source "$DKUMONITOR_DATADIR"/bin/env-default && echo "$DKUINSTALLDIR"')" != "$DKUMONITOR_INSTALLDIR" ]; then
 	# Upgrade existing data directory
 	"$DKUMONITOR_INSTALLDIR"/installer -d "$DKUMONITOR_DATADIR" -u
 fi
